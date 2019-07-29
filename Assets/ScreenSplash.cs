6c946d0ebfe220551cd957990d658157f21d42a8
Agustin Pontura <pontura@gmail.com>
2019-07-29 11:20:32 -0300

    first screens

---
+++ Assets/ScreenSplash.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenSplash : ScreenBase
+{
+    public override void OnInit()
+    {
+        print("init");
+        Invoke("Done", 3);
+    }
+    void Done()
+    {
+        MainEvents.LoadScreen(1,true);
+    }
+
+
+}
---
+++ Assets/UI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    static UI mInstance = null;
+    public ScreensManager screensManager;
+
+    public static UI Instance
+    {
+        get
+        {
+            if (mInstance == null)
+            {
+                mInstance = FindObjectOfType<UI>();
+            }
+            return mInstance;
+        }
+    }
+    void Awake()
+    {
+        if (!mInstance)
+            mInstance = this;
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+        screensManager = GetComponent<ScreensManager>();
+    }
+}
